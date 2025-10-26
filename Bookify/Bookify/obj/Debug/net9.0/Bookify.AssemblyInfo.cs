51a9103a2e34e54209a3c38e8ea083e37c560831
Amr Nabih <amrnabih112@gmail.com>
2025-10-26 20:30:51 +0300

    fixing global using

--- Bookify/Bookify/obj/Debug/net9.0/Bookify.AssemblyInfo.cs
+++ Bookify/Bookify/obj/Debug/net9.0/Bookify.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bookify")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d07631a7d4d120e42a827c805cca59ad4b5b365")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bookify")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bookify")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
