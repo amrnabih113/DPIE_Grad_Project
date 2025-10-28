03d93ee931a14b3a2c27e10843112565a950c9c3
Amr Nabih <amrnabih112@gmail.com>
2025-10-28 19:24:58 +0300

    feat: Enabling Email Validation by sending an Email

--- Bookify/Bookify/Models/EmailSender.cs
+++ Bookify/Bookify/Models/EmailSender.cs
@@ -9,14 +9,14 @@ public class EmailSender : IEmailSender
     {
         var mail = new MailMessage();
         mail.To.Add(email);
-        mail.From = new MailAddress("yourapp@gmail.com");
+        mail.From = new MailAddress("amrnabih112@gmail.com"); //  ده الميل بتاعي الي هيتبعت منه الايميلات
         mail.Subject = subject;
         mail.Body = htmlMessage;
         mail.IsBodyHtml = true;
 
         using var smtp = new SmtpClient("smtp.gmail.com", 587)
         {
-            Credentials = new NetworkCredential("yourapp@gmail.com", "your-app-password"),
+            Credentials = new NetworkCredential("amrnabih112@gmail.com", "eqmgjmuiljnwzvrv"),
             EnableSsl = true
         };
         await smtp.SendMailAsync(mail);
